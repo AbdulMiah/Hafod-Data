412b29bc5f6c072edbf0aad328e97230b200776a
Abdul Muktadir Miah <miaham@cardiff.ac.uk>
2021-03-19 14:08:37 +0000

    Fixed issue where flask wasn't connecting to database and inserting data from API. Created a .yaml file that stores the host, user, password for to access the data

--- server.py
+++ server.py
@@ -1,17 +1,94 @@
 from flask import Flask, redirect, request, render_template, url_for, jsonify
 from uk_covid19 import Cov19API #Used to call for covid case stats
-import sqlite3
-
-# import other libraries here
-#===========================
-#Importing Database
-DATABASE = 'Databases/hafod_database.db'
-#===========================
+# import sqlite3
+import mysql.connector
+import yaml
 app = Flask(__name__, template_folder='templates', static_url_path='/static', static_folder='static')
+
+
+#===========================
+# Connecting to database
+# Adapted from https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+db = yaml.load(open('db.yaml'))
+config = {
+    'host': db['mysql_host'],
+    'user': db['mysql_user'],
+    'password': db['mysql_password'],
+    'database': db['mysql_db'],
+    'raise_on_warnings': True
+}
+# #===========================
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def loadMainPage():
     return render_template('generalLayout.html', title='Hafod')
+
+
+# Sort of finished: Abdul - only adds 1 set of data
+@app.route("/ResendData", methods = ['GET', 'POST'])
+def reloadCovidFigures():
+    if request.method == 'GET':
+        ltla_areas = [
+        'areaType=ltla',
+        'date=2021-03-15'
+        ]
+
+        cases_and_deaths = {
+            "Date": "date",
+            "AreaName": "areaName",
+            "AreaType": "areaType",
+            "NewCasesByPublishDate": "newCasesByPublishDate",
+            "NewDeathsByDeathDate": "newDeathsByDeathDate"
+            }
+        #the api call
+        api = Cov19API(filters=ltla_areas, structure=cases_and_deaths)
+        #jsonifying the call
+        response = api.get_json()
+        responseInfo = response['data']
+        print(f"Data last updated:{response['lastUpdate']}")
+
+        i = 0
+        while i < len(responseInfo):
+            print(responseInfo[i])
+            apiDate = responseInfo[i]['Date']
+            apiAreaName = responseInfo[i]['AreaName']
+            apiAreaType = responseInfo[i]['AreaType']
+            NewCasesByPublishDate = responseInfo[i]['NewCasesByPublishDate']
+            NewDeathsByDeathDate = responseInfo[i]['NewDeathsByDeathDate']
+        # for i in responseInfo:
+        #     apiDate = i['Date']
+        #     apiAreaName = i['AreaName']
+        #     apiAreaType = i['AreaType']
+        #     NewCasesByPublishDate = i['NewCasesByPublishDate']
+        #     NewDeathsByDeathDate = i['NewDeathsByDeathDate']
+        #     msg = "Uploading data (Pray for it to work)"
+
+            try:
+                conn = mysql.connector.connect(**config)
+                cur = conn.cursor()
+                print("Connected to database")
+
+                query = ("INSERT INTO CovidCaseFigures "
+                        " (Date, AreaName, AreaType, NewCasesOnGivenDay, ReportedDeathsOnGivenDay) "
+                        " VALUES (%s,%s,%s,%s,%s)")
+                val = (apiDate, apiAreaName, apiAreaType, NewCasesByPublishDate, NewDeathsByDeathDate)
+                # print(val)
+
+                cur.execute(query, val)
+                msg =  "Data Set has been uploaded as a post"
+                conn.commit()
+                cur.close()
+            except conn.Error as e:
+                conn.rollback()
+                print("Ran into an error: ", e)
+            finally:
+                conn.close()
+                cur.close()
+                print(msg)
+                print("End of insertion")
+                return msg
+            i = i + 1
 
 
 ##Unfinished - Archie xx
@@ -37,7 +114,6 @@ def loadCovidFigures():
         responseInfo = response['data']
         print(f"Data last updated:{response['lastUpdate']}")
         i = 0
-        conn = sqlite3.connect(DATABASE)
         while i < len(responseInfo):
             print(responseInfo[i])
             Date = responseInfo[i]['Date']
@@ -48,10 +124,13 @@ def loadCovidFigures():
             msg = "Uploading data (Pray for it to work)"
             try:
                 conn = sqlite3.connect(DATABASE)
+                cur = conn.cursor()
                 print("Connected to database")
                 ##Error at this stage, data not being commited to SQL database
-                cur.execute("INSERT INTO covidcasefigures ('Date', 'AreaName', 'AreaType','NewCasesOnGivenDay', 'ReportedDeathsOnGivenDay')\
-                            VALUES (?,?,?,?,?)",(Date, AreaName, AreaType, NewCasesByPublishDate, NewDeathsByDeathDate) )
+                query = "INSERT INTO 'CovidCaseFigures' ('Date', 'AreaName', 'AreaType','NewCasesOnGivenDay', 'ReportedDeathsOnGivenDay')\
+                        VALUES (%s,%s,%s,%d,%d)"
+                val = [Date, AreaName, AreaType, NewCasesByPublishDate, NewDeathsByDeathDate,]
+                cur.execute(query, val)
                 print('Succesful insert')
                 conn.commit()
                 msg =  "Data Set " + i + " has been uploaded as a post"
