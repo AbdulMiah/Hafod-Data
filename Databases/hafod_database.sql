117a79bc11772c483297038d5f878e28f542118c
Archie Wilkins <wilkinsat1@cardiff.ac.uk>
2021-03-20 20:28:36 +0000

    Resolving merger issues

--- Databases/hafod_database.sql
+++ Databases/hafod_database.sql
@@ -16,15 +16,16 @@ CREATE TABLE `AdminCredentials` (
 INSERT INTO `AdminCredentials` VALUES (null, 'admin', 'admin@admin.com', 'adminpass');
 INSERT INTO `AdminCredentials` VALUES (null, 'test', 'test@test.com', 'testpass');
 INSERT INTO `AdminCredentials` VALUES (null, 'abdulmiah123', 'miaham@cardiff.ac.uk', 'abdulpass12');
-select * from AdminCredentials;
+SELECT * FROM AdminCredentials;
 
+DROP TABLE IF EXISTS `CovidCaseFigures`;
 CREATE TABLE IF NOT EXISTS `CovidCaseFigures` (
-`CasesReportID` INTEGER NOT NULL AUTO_INCREMENT,
-`Date` VARCHAR(10) NOT NULL,
-`AreaName` VARCHAR(40) NOT NULL,
-`AreaType` VARCHAR(20) NOT NULL, 
-`NewCasesOnGivenDay` INTEGER NOT NULL, 
-`ReportedDeathsOnGivenDay` INTEGER NOT NULL,
+`CasesReportID` 			INTEGER NOT NULL AUTO_INCREMENT,
+`Date` 						VARCHAR(10) NOT NULL,
+`AreaName` 					VARCHAR(40) NOT NULL,
+`AreaType` 					VARCHAR(20) NOT NULL, 
+`NewCasesOnGivenDay`  		INTEGER NOT NULL, 
+`ReportedDeathsOnGivenDay`	INTEGER,
 CONSTRAINT `PK_CasesReportID` PRIMARY KEY (`CasesReportID`)
 );
 
--- server.py
+++ server.py
@@ -1,20 +1,70 @@
 from flask import Flask, redirect, request, render_template, url_for, jsonify
 from uk_covid19 import Cov19API #Used to call for covid case stats
-import sqlite3
-
-# import other libraries here
-#===========================
-#Importing Database
-DATABASE = 'Databases/hafod_database.db'
-#===========================
+# import sqlite3
+import mysql.connector
+import yaml
 app = Flask(__name__, template_folder='templates', static_url_path='/static', static_folder='static')
+
+
+#===========================
+# Connecting to database
+# Adapted from https://dev.mysql.com/doc/connector-python/en/connector-python-example-connecting.html
+db = yaml.load(open('db.yaml'))
+config = {
+    'host': db['mysql_host'],
+    'user': db['mysql_user'],
+    'password': db['mysql_password'],
+    'database': db['mysql_db'],
+    'raise_on_warnings': True
+}
+# #===========================
+
 
 @app.route("/", methods = ['GET', 'POST'])
 def loadMainPage():
     return render_template('generalLayout.html', title='Hafod')
 
+@app.route("/Login", methods = ['GET', 'POST'])
+def loadLoginPage():
+    return redirect("static/loginPage.html")
 
-##Unfinished - Archie xx
+@app.route("/CheckLogin", methods = ['GET', 'POST'])
+def checkLoginDetails():
+    print("Validating Credentials...")
+    res = ''
+    if request.method == 'POST':
+        username = request.form.get("username", default="Error")
+        email = request.form.get("username", default="Error")
+        password = request.form.get("password", default="Error")
+        print(f"Checking details for '{username}'")
+
+        try:
+            conn = mysql.connector.connect(**config)
+            cur = conn.cursor()
+            print("Connected to database successfully")
+
+            query = ("SELECT Username, Email, Password FROM AdminCredentials "
+                    "WHERE (Username=%s OR Email=%s) AND Password=%s")
+
+            cur.execute(query, [username, email, password])
+            res = cur.fetchone()
+            msg = "Successfully fetched details from database"
+            conn.commit()
+        except conn.Error as e:
+            msg = "Failed to fetch from database"
+            print(msg,"\nError:", e)
+        finally:
+            if res:
+                print(f"Successfully logged in as: '{username}'")
+                return redirect("/")
+            else:
+                msg = "Sorry, could not find your details"
+            conn.close()
+            cur.close()
+            print(msg)
+            return msg
+
+# Abdul and Archie Peer Programming
 @app.route("/SendData", methods = ['GET', 'POST'])
 def loadCovidFigures():
     if request.method == 'GET':
@@ -37,36 +87,39 @@ def loadCovidFigures():
         responseInfo = response['data']
         print(f"Data last updated:{response['lastUpdate']}")
         i = 0
-        conn = sqlite3.connect(DATABASE)
         while i < len(responseInfo):
             print(responseInfo[i])
-            Date = responseInfo[i]['Date']
-            AreaName = responseInfo[i]['AreaName']
-            AreaType = responseInfo[i]['AreaType']
+            apiDate = responseInfo[i]['Date']
+            apiAreaName = responseInfo[i]['AreaName']
+            apiAreaType = responseInfo[i]['AreaType']
             NewCasesByPublishDate = responseInfo[i]['NewCasesByPublishDate']
             NewDeathsByDeathDate = responseInfo[i]['NewDeathsByDeathDate']
-            msg = "Uploading data (Pray for it to work)"
+            i += 1
             try:
-                conn = sqlite3.connect(DATABASE)
+                conn = mysql.connector.connect(**config)
+                cur = conn.cursor()
                 print("Connected to database")
-                ##Error at this stage, data not being commited to SQL database
-                cur.execute("INSERT INTO covidcasefigures ('Date', 'AreaName', 'AreaType','NewCasesOnGivenDay', 'ReportedDeathsOnGivenDay')\
-                            VALUES (?,?,?,?,?)",(Date, AreaName, AreaType, NewCasesByPublishDate, NewDeathsByDeathDate) )
-                print('Succesful insert')
+
+                query = ("INSERT INTO CovidCaseFigures "
+                        " (Date, AreaName, AreaType, NewCasesOnGivenDay, ReportedDeathsOnGivenDay) "
+                        " VALUES (%s,%s,%s,%s,%s)")
+                val = (apiDate, apiAreaName, apiAreaType, NewCasesByPublishDate, NewDeathsByDeathDate)
+                # print(val)
+                cur.execute(query, val)
+                msg =  "Data Set has been uploaded as a post"
                 conn.commit()
-                msg =  "Data Set " + i + " has been uploaded as a post"
-            except:
+                cur.close()
+            except conn.Error as e:
                 conn.rollback()
-                msg = "Data Set " + i + " has expierenced an error, please try again."
+                print("Ran into an error: ", e)
             finally:
                 conn.close()
+                cur.close()
+                print(msg)
                 print("End of insertion")
-                return msg
-                i = i + 1
-##End of unfinished
+        return msg
 
 
-    return render_template('generalLayout.html', title='Hafod')
 
 if __name__ == "__main__":
     app.run(debug=True)
