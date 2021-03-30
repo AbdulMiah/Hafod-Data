d908de608402120ace22ee545dfb7699e4e1bbc1
Abdul Muktadir Miah <miaham@cardiff.ac.uk>
2021-03-30 13:10:12 +0100

    Added the foreign keys to all the tables

--- Databases/Main_DB_Files/hafod_database.sql
+++ Databases/Main_DB_Files/hafod_database.sql
@@ -5,40 +5,40 @@ USE hafod_database;		-- or double click the database name in the Navigator on yo
 -- Table for tenants
 DROP TABLE IF EXISTS `tenants`;
 CREATE TABLE IF NOT EXISTS `tenants` (
-	`tenancyNo`		INTEGER NOT NULL AUTO_INCREMENT,
+	`tenancyNo`			INTEGER NOT NULL AUTO_INCREMENT,
     `healthID`			INTEGER,
     `locationID`		INTEGER,
-    `firstname`			VARCHAR(45),
-    `surname`			VARCHAR(45),
-    `dob`				DATE,
+    `firstname`			VARCHAR(45) NOT NULL,
+    `surname`			VARCHAR(45) NOT NULL,
+    `dob`				DATE NOT NULL,
     CONSTRAINT `PK_tenants` PRIMARY KEY (`tenancyNo`)
 );
 -- INSERT data into tenants
-INSERT INTO tenants VALUES(NULL, NULL, NULL, 'John', 'Doe', 2000-03-24);
-INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Sarah', 'Smith', 1988-03-20);
-INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Aria', 'Jones', 1990-06-20);
-INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Jo', 'Lee', 1970-05-27);
+INSERT INTO tenants VALUES(NULL, NULL, NULL, 'John', 'Doe', '2000-03-24');
+INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Sarah', 'Smith', '1988-03-20');
+INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Aria', 'Jones', '1990-06-20');
+INSERT INTO tenants VALUES(NULL, NULL, NULL, 'Jo', 'Lee', '1970-05-27');
 SELECT * FROM tenants;
 
 
 -- Table for carers
 DROP TABLE IF EXISTS `carers`;
 CREATE TABLE IF NOT EXISTS `carers` (
-	`staffNo`			INTEGER,
+	`staffNo`			INTEGER NOT NULL,
     `healthID`			INTEGER,
-    `firstname`			VARCHAR(45),
-    `surname`			VARCHAR(45),
-    `role`				VARCHAR(45),
-    `dob`				DATE,
     `locationID`		INTEGER,
-    CONSTRAINT `PK_carer` PRIMARY KEY (`staffNo`)
+    `firstname`			VARCHAR(45) NOT NULL,
+    `surname`			VARCHAR(45) NOT NULL,
+    `role`				VARCHAR(45) NOT NULL,
+    `dob`				DATE NOT NULL,
+    CONSTRAINT `PK_carers` PRIMARY KEY (`staffNo`)
 );
 -- INSERT data into carers
-INSERT INTO carer VALUES(741, NULL , 'Tom', 'Cooper', 'Support Assistant', 1968-03-20, NULL);
-INSERT INTO carer VALUES(8878, NULL , 'Charles', 'Osorio', 'Registered Nurse', 2000-02-05, NULL);
-INSERT INTO carer VALUES(740, NULL, 'Ffion', 'Adams', 'Registered Nurse', 1995-08-05, NULL);
-INSERT INTO carer VALUES(2345, NULL, 'Karen', 'Jenkins', 'Support Assistant', 1967-09-05, NULL);
-SELECT * FROM carer;
+INSERT INTO carers VALUES(741, NULL, NULL, 'Tom', 'Cooper', 'Support Assistant', '1968-03-20');
+INSERT INTO carers VALUES(8878, NULL, NULL, 'Charles', 'Osorio', 'Registered Nurse', '2000-02-05');
+INSERT INTO carers VALUES(740, NULL, NULL, 'Ffion', 'Adams', 'Registered Nurse', '1995-08-05');
+INSERT INTO carers VALUES(2345, NULL, NULL, 'Karen', 'Jenkins', 'Support Assistant', '1967-09-05');
+SELECT * FROM carers;
 
 
 -- Table for locations
@@ -51,7 +51,7 @@ CREATE TABLE IF NOT EXISTS `locations` (
 `localAuthority` VARCHAR(30) NOT NULL,
 `businessArea`   VARCHAR(30) NOT NULL, 
 `streetName`     VARCHAR(30),
-CONSTRAINT `PK_locationID` PRIMARY KEY (`locationID`)
+CONSTRAINT `PK_locations` PRIMARY KEY (`locationID`)
 );
 -- INSERT data into locations
 -- INSERT INTO `locations` VALUES (null, "CF23 9LJ", 228.6, 139.6, "Cardiff", "Housing", "Ael Y Bryn");
@@ -83,10 +83,10 @@ SELECT * FROM vaccinations;
 DROP TABLE IF EXISTS `covidTestResult`;
 CREATE TABLE IF NOT EXISTS `covidTestResult` (
 	`testID`				INTEGER NOT NULL AUTO_INCREMENT,
-    `positiveCase`				ENUM('yes','no') NOT NULL,
-    `status`					VARCHAR(50),
-    `resultDatee`				DATE,
-    `endOfIsolation`			DATE,
+    `positiveCase`			ENUM('yes','no') NOT NULL,
+    `status`				VARCHAR(50),
+    `resultDate`			DATE,
+    `endOfIsolation`		DATE,
     CONSTRAINT `PK_covidTestResult` PRIMARY KEY (`testID`)
 );
 -- INSERT data into covidTestResult
@@ -103,7 +103,7 @@ CREATE TABLE IF NOT EXISTS `health_linktable` (
 	`healthID`				INTEGER NOT NULL AUTO_INCREMENT,
     `vaccinationID`			INTEGER,
     `testID`				INTEGER,
-    CONSTRAINT `health_linktable` PRIMARY KEY (`healthID`)
+    CONSTRAINT `PK_health_linktable` PRIMARY KEY (`healthID`)
 );
 -- INSERT data into health_linktable
 INSERT INTO health_linktable VALUES(NULL, NULL, NULL);
@@ -111,6 +111,23 @@ INSERT INTO health_linktable VALUES(NULL, NULL, NULL);
 INSERT INTO health_linktable VALUES(NULL, NULL, NULL);
 INSERT INTO health_linktable VALUES(NULL, NULL, NULL);
 SELECT * FROM health_linktable;
+
+-- ADDING FOREIGN KEYS
+ALTER TABLE `tenants` ADD CONSTRAINT `FK_tenants_locations`
+	FOREIGN KEY (`locationID`) REFERENCES `locations` (`locationID`);
+ALTER TABLE `tenants` ADD CONSTRAINT `FK_tenants_health`
+	FOREIGN KEY (`healthID`) REFERENCES `health_linktable` (`healthID`);
+    
+ALTER TABLE `carers` ADD CONSTRAINT `FK_carers_locations`
+	FOREIGN KEY (`locationID`) REFERENCES `locations` (`locationID`);
+ALTER TABLE `carers` ADD CONSTRAINT `FK_carers_health`
+	FOREIGN KEY (`healthID`) REFERENCES `health_linktable` (`healthID`);
+    
+ALTER TABLE `health_linktable` ADD CONSTRAINT `FK_health_vaccinations`
+	FOREIGN KEY (`vaccinationID`) REFERENCES `vaccinations` (`vaccinationID`);
+ALTER TABLE `health_linktable` ADD CONSTRAINT `FK_health_covidTestResult`
+	FOREIGN KEY (`testID`) REFERENCES `covidTestResult` (`testID`);
+
 
 ------------------------------------------------
 
@@ -128,7 +145,7 @@ CREATE TABLE `AdminCredentials` (
 INSERT INTO `AdminCredentials` VALUES (null, 'admin', 'admin@admin.com', 'adminpass');
 INSERT INTO `AdminCredentials` VALUES (null, 'test', 'test@test.com', 'testpass');
 INSERT INTO `AdminCredentials` VALUES (null, 'abdulmiah123', 'miaham@cardiff.ac.uk', 'abdulpass12');
-select * from AdminCredentials;
+SELECT * FROM AdminCredentials;
 
 
 -- Table for AdminLog
@@ -138,8 +155,8 @@ CREATE TABLE IF NOT EXISTS `AdminLog` (
 `AdminID`			INTEGER,
 `TimeLoggedOn`      TIMESTAMP NOT NULL, 
 `TimeLoggedOff`     TIMESTAMP NOT NULL, 
-CONSTRAINT `FK_AdminID` FOREIGN KEY (`AdminID`) REFERENCES admincredentials(AdminID), 
-CONSTRAINT `PK_SessionID` PRIMARY KEY (`SessionID`)
+CONSTRAINT `FK_AdminCredentials` FOREIGN KEY (`AdminID`) REFERENCES admincredentials(AdminID), 
+CONSTRAINT `PK_AdminLog` PRIMARY KEY (`SessionID`)
 );
 ##Mile Stone 2 turn this into a function where admin ID is a parameter
 INSERT INTO `AdminLog` VALUES (null, 1, NOW(), NOW()+1000);
@@ -152,7 +169,7 @@ WHERE AdminID = 1);
 SELECT * FROM AdminLog;
 
 -- Table for CovidCaseFigures
-DROP TABLE `CovidCaseFigures`;
+DROP TABLE IF EXISTS `CovidCaseFigures`;
 CREATE TABLE IF NOT EXISTS `CovidCaseFigures` (
 `CasesReportID` 			INTEGER NOT NULL AUTO_INCREMENT,
 `Date` 						VARCHAR(10) NOT NULL,
@@ -160,21 +177,21 @@ CREATE TABLE IF NOT EXISTS `CovidCaseFigures` (
 `AreaType` 					VARCHAR(20) NOT NULL,
 `NewCasesOnGivenDay` 		INTEGER,
 `ReportedDeathsOnGivenDay`  INTEGER,
-CONSTRAINT `PK_CasesReportID` PRIMARY KEY (`CasesReportID`)
+CONSTRAINT `PK_CovidCaseFigures` PRIMARY KEY (`CasesReportID`)
 );
 -- INSERT data into covidcasefigures
-INSERT INTO `covidcasefigures` VALUES (null, '2021-04-03', 'Cardiff', 'ltlt', 1, Null);
+INSERT INTO `CovidCaseFigures` VALUES (null, '2021-04-03', 'Cardiff', 'ltlt', 1, Null);
 SELECT * FROM CovidCaseFigures;
 
 
 -- Table for VaccinationFigures
-DROP TABLE `VaccinationFigures`;
+DROP TABLE IF EXISTS `VaccinationFigures`;
 CREATE TABLE IF NOT EXISTS `VaccinationFigures` (
 `VaccinatedID` 			INTEGER NOT NULL AUTO_INCREMENT,
 `Date` 						VARCHAR(10) NOT NULL,
 `AreaName`					VARCHAR(40) NOT NULL,
 `AreaType` 					VARCHAR(20) NOT NULL,
-CONSTRAINT `PK_VaccinatedID` PRIMARY KEY (`VaccinatedID`)
+CONSTRAINT `PK_VaccinationFigures` PRIMARY KEY (`VaccinatedID`)
 );
 -- INSERT data into VaccinationFigures
 INSERT INTO `VaccinationFigures` VALUES (null, '2021-23-03', 'Cardiff', 'ltlt');
