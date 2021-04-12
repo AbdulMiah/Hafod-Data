890b9cb639d044ee9c72b17bba4591af390c85ca
Abdul Muktadir Miah <miaham@cardiff.ac.uk>
2021-04-12 18:44:02 +0100

    Issue #38: Created view and functions to return the total number of tenants who have COVID and number of tenants who don't have COVID

--- Databases/SQL_files/tenants_positive_cases.sql
+++ Databases/SQL_files/tenants_positive_cases.sql
@@ -1,7 +1,36 @@
 USE hafod_database;
 
+-- DROP VIEW tenantsCases;
+CREATE VIEW tenantsCases AS
 SELECT t.firstname, t.surname, t.dob, ctr.positiveCase
 FROM tenants t
 JOIN health_linktable h ON t.healthID = h.healthID
-JOIN covidTestResult ctr ON h.testID = ctr.testID
-WHERE ctr.positiveCase = 'yes';
+JOIN covidTestResult ctr ON h.testID = ctr.testID;
+SELECT * FROM tenantsCases;
+
+DELIMITER //
+CREATE FUNCTION COVIDPositiveCases()
+RETURNS VARCHAR(50)
+BEGIN
+	RETURN (
+		SELECT COUNT(*)
+		FROM tenantsCases
+		WHERE positiveCase = 'yes'
+	);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE FUNCTION COVIDNegativeCases()
+RETURNS VARCHAR(50)
+BEGIN
+	RETURN (
+		SELECT COUNT(*)
+		FROM tenantsCases
+		WHERE positiveCase = 'no'
+	);
+END //
+DELIMITER ;
+
+SELECT COVIDPositiveCases();
+SELECT COVIDNegativeCases();
