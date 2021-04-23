5b6c9566b9d68d4dadfef3b8683a02286d7ce975
Mahi Sheraj <sherajmm@cardiff.ac.uk>
2021-04-23 13:41:00 +0100

    added view to hafod script

--- Databases/Main_DB_Files/hafod_creation_script.sql
+++ Databases/Main_DB_Files/hafod_creation_script.sql
@@ -477,6 +477,16 @@ JOIN covidTestResult c ON h.testID = c.testID
 JOIN vaccinations v ON h.vaccinationID = v.vaccinationID;
 -- SELECT * FROM tenantsEditData;
 
+-- VIEW for carer data 
+CREATE VIEW adminViewOfCarersData AS 
+SELECT c.staffNo, c.firstname, c.surname, c.role, c.dob, l.postcode, l.localAuthority, l.businessArea, ctr.positiveCase, v.vaccinated
+FROM carers c
+JOIN locations l ON l.locationID = c.locationID
+JOIN health_linktable h ON c.healthID = h.healthID
+JOIN covidTestResult ctr ON h.testID = ctr.testID
+JOIN vaccinations v ON v.vaccinationID = h.vaccinationID;
+-- SELECT * FROM adminViewOfCarersData;
+
 -- STORED PROCEDURES -- --
 
 -- SP for number of tenants vaccinated
